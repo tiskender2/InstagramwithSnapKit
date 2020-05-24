1c498590fb5b46cf93b0bfa7d5a2339c0ac3c31e
Tolga İskender <tolga@basefy.com>
2020-05-24 12:43:36 +0300

    Initial Commit

---
+++ InstagramProfilewithSnapKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramProfilewithSnapKit
+//
+//  Created by Tolga İskender on 24.05.2020.
+//  Copyright © 2020 Tolga İskender. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
